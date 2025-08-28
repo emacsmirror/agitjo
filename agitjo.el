2cf98d8bbcb828a538a3b2270857e7628b60640b
Alvin Hsu <aurtzy@gmail.com>
2025-08-28 16:55:49 -0400

    Implement pushing current->upstream pull request with suffix class

--- agitjo.el
+++ agitjo.el
@@ -85,25 +85,19 @@ Thunk that returns target branch.  If nil, read from user instead.")))
 
 (defvar agitjo-post--buffer-name "*AGitjo-post*")
 
-(defvar-local agitjo-post--on-confirm-function nil
-  "Function to call when `agitjo-post-confirm' is invoked.")
-(defvar-local agitjo-post--push-args nil
-  "Buffer-local storage for transient arguments to eventually pass to \"git push\".")
+(defvar-local agitjo-post--pull-request-args nil
+  "Buffer-local storage for arguments to pass to `agitjo--push-pull-request'.")
 
 ;; TODO: Back the buffer by a file, so it can have a chance of recovery in case
 ;; of some interruption.
-(defun agitjo-post--setup-buffer (on-confirm-fun args)
+(defun agitjo-post--setup-buffer (args)
   "Set up buffer for editing pull request posts.
 
-ON-CONFIRM-FUN is the function that will be called when
-`agitjo-post-confirm' is invoked.
-
-ARGS is a list of transient arguments."
+ARGS is a list of arguments to be passed to `agitjo--push-pull-request'."
   (let* ((buffer (get-buffer-create agitjo-post--buffer-name)))
     (with-current-buffer buffer
       (agitjo-post-mode)
-      (setq agitjo-post--on-confirm-function on-confirm-fun
-            agitjo-post--push-args args)
+      (setq agitjo-post--pull-request-args args)
       (insert "\
 <!-- WARNING: Recovery facilities for this buffer have not yet been implemented.
 This buffer is NOT backed by a file, nor any history, so it is recommended to
@@ -141,31 +135,70 @@ a significant amount of content.
   (interactive)
   (unless (equal agitjo-post--buffer-name (buffer-name))
     (user-error "Function called outside AGitjo post buffer"))
-  (funcall agitjo-post--on-confirm-function))
+  (with-current-buffer agitjo-post--buffer-name
+    (apply #'agitjo--push-pull-request
+           `(,@agitjo-post--pull-request-args
+             ,(concat "--push-option=description=" (agitjo--sanitize-description
+                                                    (buffer-string)))))
+    (quit-window :kill (get-buffer-window))))
 
 ;;;; Transient suffixes.
 
 ;;;;; Auxiliary.
 
-(defun agitjo--confirm-push-current-for-upstream ()
-  "Confirm the action to push current branch for a pull request.
+(defun agitjo-get-target-branch (branch)
+  "Return the target pull request branch from BRANCH.
 
-Use the buffer's contents as a description for the PR."
-  (with-current-buffer agitjo-post--buffer-name
-    (agitjo--push-current-for-upstream
-     `(,(concat "--push-option=description=" (agitjo--sanitize-description
-                                              (buffer-string)))
-       ,agitjo-post--push-args))
-    (quit-window :kill (get-buffer-window))))
+If BRANCH is a local branch, assume that the target remote is
+`magit-primary-remote' and return the associated branch for that remote
+if it exists.  If there is no associated remote branch, return nil.
+Otherwise, BRANCH is already a remote branch, and return it as-is."
+  (if (magit-local-branch-p branch)
+      (let ((remote-branch (propertize (concat (magit-primary-remote) "/" branch)
+                                       'face 'magit-branch-remote)))
+        (if (magit-remote-branch-p remote-branch) remote-branch nil))
+    branch))
 
-(defun agitjo--push-current-for-upstream (args)
-  "Create the pull request upstream.
+(defun agitjo-pull-request-refspec (type source target)
+  "Construct and return a pull request refspec from arguments.
 
-ARGS specifies the transient arguments to be passed to \"git push\"."
-  (let* ((remote (magit-get-upstream-remote))
-         (target-branch (magit-get-local-upstream-branch))
-         (refspec (format "HEAD:refs/for/%s" target-branch)))
-    (magit-run-git-async "push" "-v" remote refspec args)))
+TYPE should be one of \"for|draft|for-review\", where \"for\" is a
+normal pull request.  This should always be \"for\", as this feature is
+not yet implemented in Forgejo.
+
+SOURCE must a local branch.  TARGET must be a remote branch."
+  (let ((valid-types '("for" "draft" "for-review")))
+    (unless (member type valid-types)
+      (error "Pull request type is not one of %S" valid-types)))
+  (unless (magit-local-branch-p source)
+    (error "Source branch is not a local branch: %S" source))
+  (let ((target-branch (agitjo--remote-branch-name target)))
+    (format "%s:refs/%s/%s/%s" source type target-branch source)))
+
+(defun agitjo--push-pull-request (type source target &rest args)
+  "Push a pull request of TYPE, from SOURCE ref to TARGET branch.
+
+TYPE, SOURCE, and TARGET will be passed to
+`agitjo-pullreq-refspec' to construct a pull request refspec; see
+for documentation.
+
+ARGS is a list of additional arguments to pass to \"git push\"."
+  (pcase-exhaustive (magit-split-branch-name target)
+    (`(,remote . ,_target-branch)
+     (let ((refspec (agitjo-pull-request-refspec type source target)))
+       (magit-run-git-async "push" "-v" remote refspec args)))))
+
+(defun agitjo--remote-branch-name (branch)
+  "Return the name part of remote branch BRANCH."
+  (unless (magit-remote-branch-p branch) (error "Not a remote branch: %S" branch))
+  (pcase-exhaustive (magit-split-branch-name branch)
+    (`(,_remote . ,name) name)))
+
+(defun agitjo--remote-branch-remote (branch)
+  "Return the remote part of remote branch BRANCH."
+  (unless (magit-remote-branch-p branch) (error "Not a remote branch: %S" branch))
+  (pcase-exhaustive (magit-split-branch-name branch)
+    (`(,remote . ,_name) remote)))
 
 (defun agitjo--sanitize-description (string)
   "Remove or convert problematic characters from description STRING."
@@ -176,23 +209,43 @@ ARGS specifies the transient arguments to be passed to \"git push\"."
 
 ;;;;; Definitions.
 
-(transient-define-suffix agitjo-push-current-for-upstream (args)
+(transient-define-suffix agitjo-push-pull-request (args)
+  "Push with AGit-Flow to create or edit a pull request.
+
+This is meant to be modified with keywords in suffix specifications.
+See `agitjo-push-pull-request-suffix' for information on slots.
+
+ARGS is a list of transient arguments to be passed to \"git push\"."
+  :class 'agitjo-push-pull-request-suffix
+  (interactive (list (transient-args 'agitjo-push)))
+  (let* ((obj (transient-suffix-object))
+         (force-push? (transient-arg-value "--push-option=force-push=true" args))
+         (source (agitjo-pull-request-source obj))
+         (target (agitjo-pull-request-target obj))
+         ;; TODO: Implement using pull request type from transient state.
+         ;; Hard-code as "for" for now.
+         (pull-request-args (list "for" source target args)))
+    (if force-push?
+        (apply #'agitjo--push-pull-request pull-request-args)
+      (agitjo-post--setup-buffer pull-request-args))))
+
+(transient-define-suffix agitjo-push-pull-request-from-current-to-upstream ()
+  :class 'agitjo-push-pull-request-suffix
+  :source #'magit-get-current-branch
+  :target (lambda ()
+            (if-let* ((branch (magit-get-upstream-branch)))
+                branch
+              ;; TODO: prompt to set upstream branch to some remote branch (we
+              ;; shouldn't include local branches).  For now, error if user
+              ;; tries to push to a nonexistent upstream.
+              (user-error "No upstream branch is set")))
   :inapt-if-not (lambda () (magit-get-current-branch))
   :description (lambda ()
                  (if-let* ((branch (magit-get-upstream-branch)))
                      branch
                    "(no upstream set)"))
-  (interactive (list (transient-args 'agitjo-push)))
-  ;; TODO: prompt to set upstream branch to some remote branch (we shouldn't
-  ;; include local branches).  For now, error if user tries to push to a
-  ;; nonexistent upstream.
-  (unless (magit-get-upstream-branch)
-    (user-error "No upstream branch is set"))
-  (let ((force-push? (transient-arg-value "--push-option=force-push=true" args)))
-    (if force-push?
-        (agitjo--push-current-for-upstream args)
-      (agitjo-post--setup-buffer
-       #'agitjo--confirm-push-current-for-upstream args))))
+  (interactive)
+  (call-interactively #'agitjo-push-pull-request))
 
 ;;;; Transient infixes.
 
@@ -254,7 +307,7 @@ that will be created or pushed to."
                                            'face 'transient-heading)
                                (propertize branch 'face 'magit-branch-local))
                      "Push <no branch set> ..."))
-    ("u" agitjo-push-current-for-upstream)]
+    ("u" agitjo-push-pull-request-from-current-to-upstream)]
   ["Configure"
    ("C" "Set variables..." magit-branch-configure)]
   (interactive)
